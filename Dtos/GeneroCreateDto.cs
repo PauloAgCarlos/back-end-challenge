f98cf705f9da21d552e07e1c71e014e1fdd2e422
Paulo A. Carlos <pauloagostinhocarlos.carlos@gmail.com>
2021-08-24 20:27:58 +0100

    Criacao das classes GeneroCreateDto e AutorCreateDto

---
+++ Dtos/GeneroCreateDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DrCashApp.Dtos
+{
+    public class GeneroCreateDto
+    {
+        [Required]
+        [MaxLength(250)]
+        public string Titulo {get;set;}
+
+        [Required]
+        public string Autor {get;set;}
+
+        [Required]
+        public string Genero {get;set;}
+
+        [Required]
+        public string QuantCopias {get;set;}
+
+         
+    }
+}
