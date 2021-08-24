ddd110bf79d783075791056f7d05c4e781a2063f
Paulo A. Carlos <pauloagostinhocarlos.carlos@gmail.com>
2021-08-24 20:09:41 +0100

    Setando as entidades Genero e Autor na classe LivroContext

--- Data/LivroContext.cs
+++ Data/LivroContext.cs
@@ -11,5 +11,7 @@ namespace DrCashApp.Data
         }
 
         public DbSet<Livro> Livros {get;set;}
+        public DbSet<Genero> Generos {get;set;}
+        public DbSet<Autor> Autores {get;set;}
     }
 }
