c38294280b69d3e62daef23df57d293688ef6414
Paulo A. Carlos <pauloagostinhocarlos.carlos@gmail.com>
2021-08-24 20:25:49 +0100

    Criacao das classes SqlGeneroRepo e SqlAutorRepo

---
+++ Data/SqlAutorRepo.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using DrCashApp.Models;
+using System.Linq;
+using System;
+
+namespace DrCashApp.Data
+{
+    public class SqlAutorRepo : IAutorRepo
+    {
+
+        private readonly AutorContext _context;
+        public SqlAutorRepo(AutorContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateAutor(Autor cmd)
+        {
+            if(cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Autores.Add(cmd);
+        }
+
+        public void DeleteAutor(Autor cmd)
+        {
+            if(cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Autores.Remove(cmd);
+        }
+
+        public IEnumerable <Autor> GetAllAutores()
+        {
+            return _context.Autores.ToList();
+        }
+
+        public Autor GetAutorById(int id)
+        {
+            return _context.Autores.FirstOrDefault(p => p.Id == id);
+        }
+ 
+        public bool SaveChanges()
+        {
+           return (_context.SaveChanges() >=0);
+        }
+
+        public void UpdateAutor(Autor cmd)
+        {
+            
+
+        }
+    }
+}
---
+++ Data/SqlGeneroRepo.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using DrCashApp.Models;
+using System.Linq;
+using System;
+
+namespace DrCashApp.Data
+{
+    public class SqlGeneroRepo : IGeneroRepo
+    {
+
+        private readonly GeneroContext _context;
+        public SqlGeneroRepo(GeneroContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateGenero(Genero cmd)
+        {
+            if(cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Generos.Add(cmd);
+        }
+
+        public void DeleteGenero(Genero cmd)
+        {
+            if(cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Generos.Remove(cmd);
+        }
+
+        public IEnumerable <Genero> GetAllGeneros()
+        {
+            return _context.Generos.ToList();
+        }
+
+        public Genero GetGeneroById(int id)
+        {
+            return _context.Generos.FirstOrDefault(p => p.Id == id);
+        }
+ 
+        public bool SaveChanges()
+        {
+           return (_context.SaveChanges() >=0);
+        }
+
+        public void UpdateGenero(Genero cmd)
+        {
+            
+
+        }
+    }
+}
